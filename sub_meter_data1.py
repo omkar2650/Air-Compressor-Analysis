4db5d56b964dc22e8a5ec17a2e3b6965f3ebbf84
omkar rane <omkar@enerlly.com>
2021-08-18 15:50:42 +0530

    First commit

--- sub_meter_data1.py
+++ sub_meter_data1.py
@@ -4,14 +4,21 @@ try:
     jsonfile = open('F:\Files send by ganesh\sub_meter_data.json', 'r')
 
 except FileNotFoundError:
-    print("FileNotFoundError")
+    print("FileNotFound")
 
 jsonData = jsonfile.read()
 obj = json.loads(jsonData)
 
 
-class AirCompressorAnalysis:
+def MaximumActivePower():
+    return obj['active_power__max'] / 1000
 
+
+def PercentageLoading():
+    return ((obj['active_power__avg'] / 1000) * obj['motor_efficiency'] / obj['rated_power']) * 100
+
+
+class AirCompressorAnalysis:
     def __init__(self, energy_received__max, energy_received__min):
         self.energy_1 = energy_received__max
         self.energy_2 = energy_received__min
@@ -20,22 +27,15 @@ class AirCompressorAnalysis:
         return self.energy_1 - self.energy_2
 
     def EnergyCost(self):
-        return Energy_Consumption * obj['tariff']
+        return self.EnergyConsumption() * obj['tariff']
 
     def GHGEmission(self):
-        return Energy_Consumption * 0.00082
-
-    def MaximumActivePower(self):
-        return obj['active_power__max'] / 1000
-
-    def PercentageLoading(self):
-        return ((obj['active_power__avg'] / 1000) * obj['motor_efficiency'] / obj['rated_power']) * 100
+        return self.EnergyConsumption() * 0.00082
 
 
 Total = AirCompressorAnalysis(obj['energy_received__max'], obj['energy_received__min'])
-Energy_Consumption = Total.EnergyConsumption()
-Energy_Cost = Total.EnergyCost()
+Total.EnergyConsumption()
+Total.EnergyCost()
 Total.GHGEmission()
-Total.MaximumActivePower()
-a = Total.PercentageLoading()
-print(a)
+MaximumActivePower()
+PercentageLoading()
